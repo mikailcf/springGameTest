bada9ab9c4affd71326e98a459e4215cbe6f9c9d
Fabio Tisovec <ftisovec@hotmail.com>
2011-09-29 14:06:27 -0300

    removendo arquivos binarios e jars do controle de versao

--- springGameTest/automatedTest/automatedTest/service/PropertyServiceTest.java
+++ springGameTest/automatedTest/automatedTest/service/PropertyServiceTest.java
@@ -2,6 +2,8 @@ package automatedTest.service;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -75,7 +77,43 @@ public class PropertyServiceTest {
 		PropertyService.addProperty(testProperties, testName,
 				new PropertyValue(testValue2),
 				PropertyCollisionAction.ExistingPrevails);
-		assertTrue(testPropertyValue.getIntValue() == testValue1 + testValue2);
+		assertTrue(testPropertyValue.getIntValue() == testValue1);
+	}
+
+	@Test
+	public final void test06() {
+		double testValue1 = 63.3;
+		double testValue2 = 24.7;
+		testPropertyValue = new PropertyValue(testValue1);
+		testProperties.addProperty(testName, testPropertyValue);
+		PropertyService.addProperty(testProperties, testName,
+				new PropertyValue(testValue2),
+				PropertyCollisionAction.ExistingPrevails);
+		assertTrue(testPropertyValue.getFloatValue() == testValue1);
+	}
+
+	@Test
+	public final void test07() {
+		String testValue1 = "bnm";
+		String testValue2 = "cxz";
+		testPropertyValue = new PropertyValue(testValue1);
+		testProperties.addProperty(testName, testPropertyValue);
+		PropertyService.addProperty(testProperties, testName,
+				new PropertyValue(testValue2),
+				PropertyCollisionAction.ExistingPrevails);
+		assertTrue(testPropertyValue.getStringValue().equals(testValue1));
+	}
+
+	@Test
+	public final void test08() {
+		Date testValue1 = new Date();
+		Date testValue2 = new Date();
+		testPropertyValue = new PropertyValue(testValue1);
+		testProperties.addProperty(testName, testPropertyValue);
+		PropertyService.addProperty(testProperties, testName,
+				new PropertyValue(testValue2),
+				PropertyCollisionAction.ExistingPrevails);
+		assertTrue(testPropertyValue.getDateValue() == testValue1);
 	}
 
 	
